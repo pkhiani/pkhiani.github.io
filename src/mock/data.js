ec6c6f008e43d4a5e9b034bed2944108f2d8f5c6
pkhiani <pkhiani@students.cs.ubc.ca>
2021-05-07 16:38:31 -0600

    updated project section

--- src/mock/data.js
+++ src/mock/data.js
@@ -28,30 +28,30 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'eyespy.png',
+    title: 'EyeSpy',
+    info: 'An application designed to reduce eye strain while looking at a computer screen. It utilizes computer vision to count the number of times the user blinks during their work session and determines whether they are blinking a healthy ammount. In addition, the application queues eye exercises once a user is on their break to relieve eye strain.',
+    info2: 'Created using Python, Google Firebase, OpenCV',
+    url: 'https://devpost.com/software/eyespy-fowtyn',
+    repo: 'https://github.com/pkhiani/eyespy', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'borb.png',
+    title: 'Bearish or Bullish',
+    info: 'A Web Application that performs a Sentiment Analysis of recent news of the past few days on any given company listed on the stock exchange that the user inputs',
+    info2: 'Created Using Python, Django',
+    url: 'https://devpost.com/software/bearish-or-bullish',
+    repo: 'https://github.com/pkhiani/bearish-or-bullish', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'auction.png',
+    title: 'AuctionApp',
+    info: 'A GUI application that has different cars that are available for auction that the user can view, and can place a bid on if they choose to. Some features include a system to check if you won the bid, or if you want to put your own listing up for auction.',
+    info2: 'Created using Java',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/pkhiani/AuctionApp', // if no repo, the button will not show up
   },
 ];
 
