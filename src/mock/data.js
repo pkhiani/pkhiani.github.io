a829a0dac06753cc4a0b055d7314771d620421bd
Pavan Khiani <pavan.khiani@gmail.com>
2021-08-18 13:56:00 -0700

    updated projects section

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,9 +18,12 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profilepic.png',
-  paragraphOne: 'Hi there! I am Pavan Khiani, and I am currently studying Business and Computer Science at the University of British Columbia!',
-  paragraphTwo: 'I am currently exploring new opportunities in Technology Consulting, Product Management, and Data Analytics',
-  paragraphThree: 'Outside of the classroom, you can find me hiking, skiing, or doing many outdoors sports. I am also involved in the community at UBC',
+  paragraphOne:
+    'Hi there! I am Pavan Khiani, and I am currently studying Business and Computer Science at the University of British Columbia!',
+  paragraphTwo:
+    'I am currently exploring new opportunities in Cloud Technologies, Data Analytics, and Product Management',
+  paragraphThree:
+    'Outside of the classroom, you can find me hiking, skiing, or doing many outdoors sports. I am also involved in the community at UBC',
   resume: 'https://drive.google.com/file/d/1nvqm6ZsIQxgR2dZucgBh40eyz5wbpWK6/view?usp=sharing', // if no resume, the button will not show up
 };
 
@@ -28,9 +31,20 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
+    img: 'clubfinder.png',
+    title: 'ClubFinder',
+    info:
+      'A web application that displays results of extra curriculars for parents and high school students in the area, based on one of the various categories selected',
+    info2: 'Created using Python, Django, HTML/CSS, Microsoft Azure',
+    url: 'https://clubfinder.azurewebsites.net',
+    repo: 'https://github.com/pkhiani/clubfinder', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
     img: 'eyespy.png',
     title: 'EyeSpy',
-    info: 'An application designed to reduce eye strain while looking at a computer screen. It utilizes computer vision to count the number of times the user blinks during their work session and determines whether they are blinking a healthy ammount. In addition, the application queues eye exercises once a user is on their break to relieve eye strain.',
+    info:
+      'An application designed to reduce eye strain while looking at a computer screen. It utilizes computer vision to count the number of times the user blinks during their work session and determines whether they are blinking a healthy ammount. In addition, the application queues eye exercises once a user is on their break to relieve eye strain.',
     info2: 'Created using Python, Google Firebase, OpenCV',
     url: 'https://devpost.com/software/eyespy-fowtyn',
     repo: 'https://github.com/pkhiani/eyespy', // if no repo, the button will not show up
@@ -39,7 +53,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'borb.jpg',
     title: 'Bearish or Bullish',
-    info: 'A Web Application that performs a Sentiment Analysis of recent news of the past few days on any given company listed on the stock exchange that the user inputs',
+    info:
+      'A web Application that performs a Sentiment Analysis of recent news of the past few days on any given company listed on the stock exchange that the user inputs',
     info2: 'Created Using Python, Django',
     url: 'https://devpost.com/software/bearish-or-bullish',
     repo: 'https://github.com/pkhiani/bearish-or-bullish', // if no repo, the button will not show up
@@ -48,7 +63,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'auction.png',
     title: 'AuctionApp',
-    info: 'A GUI application that has different cars that are available for auction that the user can view, and can place a bid on if they choose to. Some features include a system to check if you won the bid, or if you want to put your own listing up for auction.',
+    info:
+      'A GUI application that has different cars that are available for auction that the user can view, and can place a bid on if they choose to. Some features include a system to check if you won the bid, or if you want to put your own listing up for auction.',
     info2: 'Created using Java',
     url: '',
     repo: 'https://github.com/pkhiani/AuctionApp', // if no repo, the button will not show up
