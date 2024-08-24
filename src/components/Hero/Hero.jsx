4945d2dd9d7068b023343f7edb644dd6dc15ac9a
Pavan Khiani <65191926+pkhiani@users.noreply.github.com>
2024-08-24 17:47:39 -0600

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,7 +27,7 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi! My name is'}{' '}
-            <span className="text-color-main">{name || 'Pavan'}</span>
+            <span className="text-color-main">{name || 'Pavan.'}</span>
             <br />
             {/* {subtitle || "I'm a Product Manager and Business + Computer Science graduate."} */}
           </h1>
