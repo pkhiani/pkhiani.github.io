27e183a7bc678e2b031f841bc91e4e095b363d5d
Pavan Khiani <pavan.khiani@gmail.com>
2024-10-06 15:06:48 -0600

    homepage image

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -100,7 +100,7 @@ const Homepage = () => {
 								<div className="homepage-image-container">
 									<div className="homepage-image-wrapper">
 										<img
-											src="hi.png"
+											src="laptop cat.png"
 											alt="about"
 											className="homepage-image"
 										/>
